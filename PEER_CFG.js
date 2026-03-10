d70eaf9cca13e592daa9c0f5a8536a3d742abe9e
KingHead00 <rufaelrufael1@gmail.com>
2026-03-09 20:45:08 -0400

    Create PEER_CFG.js

---
+++ PEER_CFG.js
@@ -0,0 +1,6 @@
+const PEER_CFG = { config: { iceServers: [
+  { urls: 'stun:stun.l.google.com:19302' },
+  { urls: 'stun:stun1.l.google.com:19302' },
+  { urls: 'stun:stun2.l.google.com:19302' },
+  { urls: 'stun:global.stun.twilio.com:3478' }
+]}};
